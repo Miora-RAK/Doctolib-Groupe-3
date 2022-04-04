a11862e8736a6d5a177b6567c807e11aa1cbeff4
laurent dubois <laurent.dubois@leroymerlin.fr>
2022-04-04 15:13:41 +0200

    add auth0 and login/logout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+// pages/_app.js
+import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
-export default MyApp
+export default function App({ Component, pageProps }:AppProps): React.ReactElement<AppProps> {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
+}
