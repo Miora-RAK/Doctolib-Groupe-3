246b95d55c53bdce2a8f92e63c99781a102ccbef
Nordine El yazidi <78361263+nordine-yzd@users.noreply.github.com>
2022-04-05 10:04:51 +0200

    Update show-users.tsx

--- pages/show-users.tsx
+++ pages/show-users.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useUser } from "@auth0/nextjs-auth0";
 import { Layout } from "../components/Layout";
-import styles from "../styles/Home.module.css";
 import Image from "next/image";
 
 export default function Profile() {
