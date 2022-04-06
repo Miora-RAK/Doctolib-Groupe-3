d38c3f860f5dccb06215d3879dd5947f3cbaabb8
Lucas Gillon <lucas.gillon98@gmail.com>
2022-04-06 14:10:08 +0200

    récupétation des données du form + console.log de ces données

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -1,31 +1,105 @@
+import React from "react";
 import { Layout } from "../components/Layout";
 
 const Forms: React.FC = () => {
-  return (
-    <>
-      <Layout>
-        <main>
-          <form action="#">
-            <label>
-              <input type="text" name="name" placeholder="name" />
-            </label>
-            <label>
-              <input type="text" name="email" placeholder="email" />
-            </label>
+  const [status, setStatus] = React.useState();
+  const handleChange = (e: { target: any }) => {
+    const target = e.target;
+    if (target.checked) {
+      setStatus(target.value);
+    }
+  };
+  const handleText = (e: any) => {
+    // console.log(e.target.value);
+  };
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    let user;
+    if (status === "Patient") {
+      user = {
+        name: e.target[0].value,
+        email: e.target[1].value,
+        status: status,
+      };
+    } else {
+      user = {
+        name: e.target[0].value,
+        email: e.target[1].value,
+        status: status,
+        disponibilities: e.target[4].value,
+      };
+    }
 
-            <div>
-              <input type="radio" name="drone" value="patient" />
-              <label htmlFor="name">Patient</label>
-            </div>
-            <div>
-              <input type="radio" name="drone" value="doctor" />
-              <label htmlFor="email">Doctor</label>
-            </div>
-            <button> Envoyer </button>
-          </form>
-        </main>
-      </Layout>
-    </>
+    console.log(user);
+  };
+
+  return (
+    <Layout>
+      <main>
+        <form onSubmit={handleSubmit}>
+          <h2
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              paddingBottom: "3em",
+            }}
+          >
+            Signup Form
+          </h2>
+          <label>
+            <input
+              type="text"
+              name="name"
+              placeholder="name"
+              onChange={handleText}
+            />
+          </label>
+          <br />
+          <label>
+            <input
+              type="text"
+              name="email"
+              placeholder="email"
+              onChange={handleText}
+            />
+          </label>
+          <br />
+          <br />
+          <div>
+            <input
+              type="radio"
+              name="drone"
+              value="Patient"
+              onChange={handleChange}
+            />
+            <label htmlFor="name"> Patient</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="drone"
+              value="Doctor"
+              onChange={handleChange}
+            />
+            <label htmlFor="email"> Doctor</label>
+          </div>
+          <br />
+          <div>
+            <label htmlFor="disponibilities">
+              If you are a doctor, please indicate your disponibilities :
+            </label>
+            <br />
+            <input
+              type="text"
+              name="drone"
+              placeholder="Disponibilities"
+              onChange={handleChange}
+            />
+          </div>
+          <button type="submit">Envoyer</button>
+        </form>
+      </main>
+    </Layout>
   );
 };
 
